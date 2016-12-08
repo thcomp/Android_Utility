70a2dea006c6c52165a9e9ee4d024d52fcb4718a
EQ_htatsuguchi <tatsuguchi@eq-inc.jp>
2016-12-09 08:35:24 +0900

    v1.0.14: OnetimeSemaphoreを追加

--- utility/src/main/java/jp/co/thcomp/util/ThreadUtil.java
+++ utility/src/main/java/jp/co/thcomp/util/ThreadUtil.java
@@ -102,4 +102,45 @@ public class ThreadUtil {
             }
         }, delayTimeMS);
     }
+
+    /**
+     * ワンタイムセマフォ
+     * startをコールする前にstopがコールされた場合にstartにて停止し続けないセマフォ
+     * ※ waitをコールする前にリスナーを登録する機能を実施すると稀に実施した中で同一スレッド上でリスナーが
+     * コールされ、そのタイミングでnotifyされてしまうと、その後waitし続けてしまうものを解消するのが目的
+     * 再利用する場合はinitializeを実施すること
+     */
+    public static class OnetimeSemaphore {
+        private boolean stopped = false;
+
+        public OnetimeSemaphore() {
+            initialize();
+        }
+
+        public void start() {
+            synchronized (this) {
+                if (!stopped) {
+                    try {
+                        this.wait();
+                    } catch (InterruptedException e) {
+                    }
+                } else {
+                    // 既に停止されている場合は、waitは掛けずに終了
+                }
+            }
+        }
+
+        public void stop() {
+            synchronized (this) {
+                if (!stopped) {
+                    this.notify();
+                    stopped = true;
+                }
+            }
+        }
+
+        public void initialize() {
+            stopped = false;
+        }
+    }
 }
