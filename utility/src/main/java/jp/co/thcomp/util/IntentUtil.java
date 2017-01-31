f0628392f8abb9e6298adbb5ebdea39d6b706afd
EQ_htatsuguchi <tatsuguchi@eq-inc.jp>
2017-02-01 08:58:34 +0900

    v1.0.19 添付ファイルをサポート

--- utility/src/main/java/jp/co/thcomp/util/IntentUtil.java
+++ utility/src/main/java/jp/co/thcomp/util/IntentUtil.java
@@ -3,6 +3,8 @@ package jp.co.thcomp.util;
 import android.content.Intent;
 import android.net.Uri;
 
+import java.io.File;
+
 public class IntentUtil {
     public static Intent getLaunchMailerIntent(String toAddress, String title, String message){
         Intent intent = new Intent();
@@ -17,6 +19,20 @@ public class IntentUtil {
         intent.putExtra(Intent.EXTRA_SUBJECT, title);
         intent.putExtra(Intent.EXTRA_TEXT, message);
 
+        return intent;
+    }
+
+    public static Intent getLaunchMailerIntent(String toAddress, String title, String message, String attachmentFilePath){
+        return getLaunchMailerIntent(toAddress, title, message, new File(attachmentFilePath));
+    }
+
+    public static Intent getLaunchMailerIntent(String toAddress, String title, String message, File attachmentFile){
+        return getLaunchMailerIntent(toAddress, title, message, Uri.fromFile(attachmentFile));
+    }
+
+    public static Intent getLaunchMailerIntent(String toAddress, String title, String message, Uri attachmentFileUri){
+        Intent intent = getLaunchMailerIntent(toAddress, title, message);
+        intent.putExtra(Intent.EXTRA_STREAM, attachmentFileUri);
         return intent;
     }
 }
