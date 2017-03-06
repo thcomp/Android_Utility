56206a2509c426c0fadc4956e427562f46324722
EQ_htatsuguchi <tatsuguchi@eq-inc.jp>
2017-03-06 16:44:19 +0900

    v1.0.21 RuntimePermissionUtilの追加 ThreadUtil.OnetimeSemaphore.start(long timeout)を追加

--- utility/src/main/java/jp/co/thcomp/util/RuntimePermissionUtil.java
+++ utility/src/main/java/jp/co/thcomp/util/RuntimePermissionUtil.java
@@ -6,16 +6,15 @@ import android.os.Build;
 import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
 
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
 
-public class RuntimePermissionManager {
+public class RuntimePermissionUtil {
     public interface OnRequestPermissionsResultListener {
         public void onRequestPermissionsResult(@NonNull String[] permissions, @NonNull int[] grantResults);
     }
 
-    private HashMap<Integer, OnRequestPermissionsResultListener> mRequestCodeListenerMap = new HashMap<Integer, OnRequestPermissionsResultListener>();
+    private static final HashMap<Integer, OnRequestPermissionsResultListener> sRequestCodeListenerMap = new HashMap<Integer, OnRequestPermissionsResultListener>();
 
     /**
      * 実行時パーミッション許可のリクエスト送信
@@ -23,12 +22,10 @@ public class RuntimePermissionManager {
      * @param activity
      * @param permissions
      */
-    public void requestPermission(final Activity activity, final String[] permissions, final OnRequestPermissionsResultListener listener) {
+    public static void requestPermissions(final Activity activity, final String[] permissions, final OnRequestPermissionsResultListener listener) {
         boolean needRequestPermission = false;
 
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-            ArrayList<String> needRequestPermissionList = new ArrayList<String>();
-
             for (String permission : permissions) {
                 if (ActivityCompat.checkSelfPermission(activity, permission) == PackageManager.PERMISSION_DENIED) {
                     needRequestPermission = true;
@@ -38,7 +35,9 @@ public class RuntimePermissionManager {
 
             if (needRequestPermission) {
                 int requestCode = permissions.hashCode() & 0x0000FFFF;
-                mRequestCodeListenerMap.put(requestCode, listener);
+                synchronized (sRequestCodeListenerMap) {
+                    sRequestCodeListenerMap.put(requestCode, listener);
+                }
                 ActivityCompat.requestPermissions(activity, permissions, requestCode);
             }
         }
@@ -65,8 +64,12 @@ public class RuntimePermissionManager {
      * @param grantResults
      * @return true: 処理済み、false: 未処理
      */
-    public boolean onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
-        OnRequestPermissionsResultListener listener = mRequestCodeListenerMap.remove(requestCode);
+    public static boolean onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        OnRequestPermissionsResultListener listener = null;
+        synchronized (sRequestCodeListenerMap){
+            listener = sRequestCodeListenerMap.remove(requestCode);
+        }
+
         if (listener != null) {
             listener.onRequestPermissionsResult(permissions, grantResults);
             return true;
--- utility/src/main/java/jp/co/thcomp/util/ThreadUtil.java
+++ utility/src/main/java/jp/co/thcomp/util/ThreadUtil.java
@@ -7,27 +7,27 @@ import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 
 public class ThreadUtil {
-    public static boolean IsOnMainThread(Context context){
+    public static boolean IsOnMainThread(Context context) {
         return Thread.currentThread().equals(context.getMainLooper().getThread());
     }
 
-    public static void runOnMainThread(final Context context, final Runnable runnable){
-        if(IsOnMainThread(context)){
+    public static void runOnMainThread(final Context context, final Runnable runnable) {
+        if (IsOnMainThread(context)) {
             runnable.run();
-        }else{
+        } else {
             Handler handler = new Handler(context.getMainLooper());
             handler.post(runnable);
         }
     }
 
-    public static void runOnMainThread(final Context context, final Object targetObject, final Method method, final Object... params){
-        if(IsOnMainThread(context)){
+    public static void runOnMainThread(final Context context, final Object targetObject, final Method method, final Object... params) {
+        if (IsOnMainThread(context)) {
             try {
                 method.invoke(targetObject, params);
             } catch (IllegalAccessException e) {
             } catch (InvocationTargetException e) {
             }
-        }else{
+        } else {
             Handler handler = new Handler(context.getMainLooper());
             handler.post(new Runnable() {
                 @Override
@@ -38,12 +38,12 @@ public class ThreadUtil {
         }
     }
 
-    public static void delayRunOnMainThread(final Context context, final long delayTimeMS, final Runnable runnable){
+    public static void delayRunOnMainThread(final Context context, final long delayTimeMS, final Runnable runnable) {
         Handler handler = new Handler(context.getMainLooper());
         handler.postDelayed(runnable, delayTimeMS);
     }
 
-    public static void delayRunOnMainThread(final Context context, final long delayTimeMS, final Object targetObject, final Method method, final Object... params){
+    public static void delayRunOnMainThread(final Context context, final long delayTimeMS, final Object targetObject, final Method method, final Object... params) {
         Handler handler = new Handler(context.getMainLooper());
         handler.postDelayed(new Runnable() {
             @Override
@@ -53,23 +53,23 @@ public class ThreadUtil {
         }, delayTimeMS);
     }
 
-    public static void runOnWorkThread(final Context context, final Runnable runnable){
-        if(IsOnMainThread(context)){
+    public static void runOnWorkThread(final Context context, final Runnable runnable) {
+        if (IsOnMainThread(context)) {
             new Thread(runnable).start();
-        }else{
+        } else {
             runnable.run();
         }
     }
 
-    public static void runOnWorkThread(final Context context, final Object targetObject, final Method method, final Object... params){
-        if(IsOnMainThread(context)){
+    public static void runOnWorkThread(final Context context, final Object targetObject, final Method method, final Object... params) {
+        if (IsOnMainThread(context)) {
             new Thread(new Runnable() {
                 @Override
                 public void run() {
                     runOnWorkThread(context, targetObject, method, params);
                 }
             }).start();
-        }else{
+        } else {
             try {
                 method.invoke(targetObject, params);
             } catch (IllegalAccessException e) {
@@ -78,7 +78,7 @@ public class ThreadUtil {
         }
     }
 
-    public static void delayRunOnWorkThread(final Context context, final long delayTimeMS, final Runnable runnable){
+    public static void delayRunOnWorkThread(final Context context, final long delayTimeMS, final Runnable runnable) {
         Handler handler = new Handler(context.getMainLooper());
         handler.postDelayed(new Runnable() {
             @Override
@@ -88,7 +88,7 @@ public class ThreadUtil {
         }, delayTimeMS);
     }
 
-    public static void delayRunOnWorkThread(final Context context, final long delayTimeMS, final Object targetObject, final Method method, final Object... params){
+    public static void delayRunOnWorkThread(final Context context, final long delayTimeMS, final Object targetObject, final Method method, final Object... params) {
         Handler handler = new Handler(context.getMainLooper());
         handler.postDelayed(new Runnable() {
             @Override
@@ -128,6 +128,25 @@ public class ThreadUtil {
                     // 既に停止されている場合は、waitは掛けずに終了
                 }
             }
+        }
+
+        public boolean start(long timeout) {
+            boolean ret = true;
+
+            synchronized (this) {
+                if (!stopped) {
+                    try {
+                        this.wait(timeout);
+                        ret = stopped;
+                    } catch (InterruptedException e) {
+                        ret = false;
+                    }
+                } else {
+                    // 既に停止されている場合は、waitは掛けずに終了
+                }
+            }
+
+            return ret;
         }
 
         public void stop() {
