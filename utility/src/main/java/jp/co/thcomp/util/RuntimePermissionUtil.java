fb6e2b4031f51cf6083dc4a936fc244bb40a229b
EQ_htatsuguchi <tatsuguchi@eq-inc.jp>
2018-01-11 15:20:23 +0900

    サポートライブラリの引き込みを排除

--- utility/src/main/java/jp/co/thcomp/util/RuntimePermissionUtil.java
+++ utility/src/main/java/jp/co/thcomp/util/RuntimePermissionUtil.java
@@ -1,20 +1,38 @@
 package jp.co.thcomp.util;
 
 import android.app.Activity;
+import android.content.Context;
 import android.content.pm.PackageManager;
 import android.os.Build;
-import android.support.annotation.NonNull;
-import android.support.v4.app.ActivityCompat;
 
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 import java.util.Arrays;
 import java.util.HashMap;
 
 public class RuntimePermissionUtil {
-    public interface OnRequestPermissionsResultListener {
-        public void onRequestPermissionsResult(@NonNull String[] permissions, @NonNull int[] grantResults);
+    private static Method ActivityCompatCheckSelfPermission = null;
+    private static Method ActivityCompatRequestPermissions = null;
+
+    static{
+        try {
+            Class activityCompatCl = Class.forName("android.support.v4.app.ActivityCompat");
+            Class contextCompatCl = Class.forName("android.support.v4.content.ContextCompat");
+            ActivityCompatCheckSelfPermission = contextCompatCl.getMethod("checkSelfPermission", Context.class, String.class);
+            ActivityCompatRequestPermissions = activityCompatCl.getMethod("requestPermissions", Activity.class, String[].class, int.class);
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        } catch (NoSuchMethodException e) {
+            e.printStackTrace();
+        }
     }
 
-    private static final HashMap<Integer, OnRequestPermissionsResultListener> sRequestCodeListenerMap = new HashMap<Integer, OnRequestPermissionsResultListener>();
+    public interface OnRequestPermissionsResultListener {
+        public void onRequestPermissionsResult(String[] permissions, int[] grantResults);
+    }
+
+    private static byte sRequestCode = 0;
+    private static final HashMap<Integer, OnRequestPermissionsResultListener> sRequestCodeListenerMap = new HashMap<>();
 
     /**
      * 実行時パーミッション許可のリクエスト送信
@@ -27,18 +45,24 @@ public class RuntimePermissionUtil {
 
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
             for (String permission : permissions) {
-                if (ActivityCompat.checkSelfPermission(activity, permission) == PackageManager.PERMISSION_DENIED) {
+                if (checkSelfPermission(activity, permission) == PackageManager.PERMISSION_DENIED) {
                     needRequestPermission = true;
                     break;
                 }
             }
 
             if (needRequestPermission) {
-                int requestCode = permissions.hashCode() & 0x0000FFFF;
-                synchronized (sRequestCodeListenerMap) {
-                    sRequestCodeListenerMap.put(requestCode, listener);
+                int requestCode;
+                synchronized (sRequestCodeListenerMap){
+                    requestCode = sRequestCode;
+                    sRequestCodeListenerMap.put((int)sRequestCode, listener);
+                    if(sRequestCode == Byte.MAX_VALUE){
+                        sRequestCode = 0;
+                    }
+                    sRequestCode++;
                 }
-                ActivityCompat.requestPermissions(activity, permissions, requestCode);
+
+                requestPermissions(activity, permissions, requestCode);
             }
         }
 
@@ -64,7 +88,7 @@ public class RuntimePermissionUtil {
      * @param grantResults
      * @return true: 処理済み、false: 未処理
      */
-    public static boolean onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+    public static boolean onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
         OnRequestPermissionsResultListener listener = null;
         synchronized (sRequestCodeListenerMap){
             listener = sRequestCodeListenerMap.remove(requestCode);
@@ -76,5 +100,31 @@ public class RuntimePermissionUtil {
         }
 
         return false;
+    }
+
+    private static int checkSelfPermission(Activity activity, String permission){
+        int ret = PackageManager.PERMISSION_DENIED;
+
+        try {
+            // ActivityCompatがリンクされていない場合、ActivityCompatCheckSelfPermissionがnullのため、NullPointerExceptionが通知されて終了させる想定
+            ret = (int)ActivityCompatCheckSelfPermission.invoke(null, activity, permission);
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        } catch (InvocationTargetException e) {
+            e.printStackTrace();
+        }
+
+        return ret;
+    }
+
+    private static void requestPermissions(Activity activity, String[] permissions, int requestCode){
+        try {
+            // ActivityCompatがリンクされていない場合、ActivityCompatRequestPermissionsがnullのため、NullPointerExceptionが通知されて終了させる想定
+            ActivityCompatRequestPermissions.invoke(null, activity, permissions, requestCode);
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        } catch (InvocationTargetException e) {
+            e.printStackTrace();
+        }
     }
 }
